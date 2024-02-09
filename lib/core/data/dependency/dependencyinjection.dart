d518d81f7016ec680595a14c956b7929a85511b2
Piton <11895336+manbbo@users.noreply.github.com>
2024-02-09 20:03:33 -0300

    Resolving dependencies

--- lib/core/data/dependency/dependencyinjection.dart
+++ lib/core/data/dependency/dependencyinjection.dart
@@ -1,7 +1,8 @@
 import 'package:injectable/injectable.dart';
 import 'package:get_it/get_it.dart';
+import 'package:nomabe/core/data/dependency/dependencyinjection.config.dart';
 
 final GetIt getIt = GetIt.instance;
 
 @injectableInit
-void configureDependencies() => $initGetIt(getIt);
+void configureDependencies() => getIt.init();
